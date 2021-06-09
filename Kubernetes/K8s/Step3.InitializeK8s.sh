5d9d9361e0bd2465756fba3a4e3d6d85e8be6fdc
Hồ Thanh Nam <58923848+nShieldSolo@users.noreply.github.com>
2021-06-09 16:33:55 +0700

    add

--- Kubernetes/K8s/Step3.InitializeK8s.sh
+++ Kubernetes/K8s/Step3.InitializeK8s.sh
@@ -1,6 +1,6 @@
 echo "================ Initialize Kubernetes Master and Setup Default User ================="
 swapoff -a
-kubeadm init
+kubeadm init --apiserver-advertise-address=10.0.4.221 --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
