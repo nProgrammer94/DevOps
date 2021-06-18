cbd3312e353dd817ef1b054836b33441f4932f3a
Hồ Thanh Nam <58923848+nShieldSolo@users.noreply.github.com>
2021-06-18 14:39:02 +0700

    Finish

--- Jenkins/setup.sh
+++ Jenkins/setup.sh
@@ -12,5 +12,18 @@ sudo yum upgrade -y
 sudo yum install -y jenkins java-11-openjdk-devel
 sudo systemctl daemon-reload
 
-# SSH
-sudo docker exec -u root -it <containerName> /bin/bash
+# Install K8s
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+sudo yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet
+systemctl start kubelet
